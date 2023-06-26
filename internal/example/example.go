2313cadfe7a9fb175b1dee1045bb0831ac8b483b
Will Roden <will@roden.cc>
2023-06-26 11:56:48 -0500

    update example

--- internal/example/example.go
+++ internal/example/example.go
@@ -27,9 +27,7 @@ func main() {
 
 	logger.Debug("this is a debug message")
 
-	logger.Info("this is a\nmultiline\nmessage")
-
-	logger.Info("goodbye", slog.String("multiline value", "this is a\nmultiline\nvalue"))
+	logger.Info("this is a\nmultiline\nmessage", slog.String("val", "this is a\nmultiline\nvalue"))
 
 	noSourceLogger := slog.New(actionslog.New(os.Stdout, nil))
 	noSourceLogger.Info("this log line has no source")
